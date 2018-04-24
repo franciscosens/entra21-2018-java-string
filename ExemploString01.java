71856249756dc771d78e4d92a5d3b403c8450053
Francisco Lucas Sens <francisco.lucas.sens@gmail.com>
2018-04-24 10:53:12 -0300

    Inicialização do repositório

---
+++ ExemploString01.java
@@ -0,0 +1,13 @@
+public class ExemploString01{
+    public static void main(String[] args) {
+
+        String nome = "Curso de Java";
+        System.out.println(nome);
+
+        // retorna a quantidade de caracteres da String
+        int tamanhoDaString = nome.length();
+
+        System.out.println("Tamanho da String: " + tamanhoDaString);
+
+    }
+}
